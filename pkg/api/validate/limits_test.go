3bd519fb11325df5e6e9b14c9a931a30d91e99c5
Tim Hockin <thockin@google.com>
2026-03-10 10:58:39 -0700

    Fix minimum test to match maximum with ErrorMatcher
    Kubernetes-commit: 7f90ee00434a1d8572a2cb1fb0a81c12a988f409

--- pkg/api/validate/limits_test.go
+++ pkg/api/validate/limits_test.go
@@ -192,27 +192,31 @@ func TestMinimum(t *testing.T) {
 }
 
 type minimumTestCase[T constraints.Integer] struct {
-	value T
-	min   T
-	err   string // regex
+	min      T
+	value    T
+	wantErrs field.ErrorList
 }
 
 func testMinimumPositive[T constraints.Integer](t *testing.T) {
 	t.Helper()
 	cases := []minimumTestCase[T]{{
-		value: 0,
 		min:   0,
-	}, {
 		value: 0,
-		min:   1,
-		err:   "fldpath: Invalid value.*must be greater than or equal to",
 	}, {
-		value: 1,
 		min:   1,
+		value: 0,
+		wantErrs: field.ErrorList{
+			field.Invalid(field.NewPath("fldpath"), nil, "must be greater than or equal to").WithOrigin("minimum"),
+		},
 	}, {
+		min:   1,
 		value: 1,
+	}, {
 		min:   2,
-		err:   "fldpath: Invalid value.*must be greater than or equal to",
+		value: 1,
+		wantErrs: field.ErrorList{
+			field.Invalid(field.NewPath("fldpath"), nil, "must be greater than or equal to").WithOrigin("minimum"),
+		},
 	}}
 	doTestMinimum[T](t, cases)
 }
@@ -220,12 +224,14 @@ func testMinimumPositive[T constraints.Integer](t *testing.T) {
 func testMinimumNegative[T constraints.Signed](t *testing.T) {
 	t.Helper()
 	cases := []minimumTestCase[T]{{
+		min:   -1,
 		value: -1,
-		min:   -1,
 	}, {
-		value: -2,
 		min:   -1,
-		err:   "fldpath: Invalid value.*must be greater than or equal to",
+		value: -2,
+		wantErrs: field.ErrorList{
+			field.Invalid(field.NewPath("fldpath"), nil, "must be greater than or equal to").WithOrigin("minimum"),
+		},
 	}}
 
 	doTestMinimum[T](t, cases)
@@ -233,26 +239,14 @@ func testMinimumNegative[T constraints.Signed](t *testing.T) {
 
 func doTestMinimum[T constraints.Integer](t *testing.T, cases []minimumTestCase[T]) {
 	t.Helper()
-	for i, tc := range cases {
-		v := tc.value
-		result := Minimum(context.Background(), operation.Operation{}, field.NewPath("fldpath"), &v, nil, tc.min)
-		if len(result) > 0 && tc.err == "" {
-			t.Errorf("case %d: unexpected failure: %v", i, fmtErrs(result))
-			continue
-		}
-		if len(result) == 0 && tc.err != "" {
-			t.Errorf("case %d: unexpected success: expected %q", i, tc.err)
-			continue
-		}
-		if len(result) > 0 {
-			if len(result) > 1 {
-				t.Errorf("case %d: unexepected multi-error: %v", i, fmtErrs(result))
-				continue
-			}
-			if re := regexp.MustCompile(tc.err); !re.MatchString(result[0].Error()) {
-				t.Errorf("case %d: wrong error\nexpected: %q\n     got: %v", i, tc.err, fmtErrs(result))
-			}
-		}
+	matcher := field.ErrorMatcher{}.ByOrigin().ByDetailSubstring().ByField().ByType()
+	for _, tc := range cases {
+		name := fmt.Sprintf("%T (%v >= %v)", tc.value, tc.value, tc.min)
+		t.Run(name, func(t *testing.T) {
+			v := tc.value
+			gotErrs := Minimum(context.Background(), operation.Operation{}, field.NewPath("fldpath"), &v, nil, tc.min)
+			matcher.Test(t, tc.wantErrs, gotErrs)
+		})
 	}
 }
 
